c6e93e941292645cc5910f4f8e33ea96297efded
holifieldw <holifield04@gmail.com>
2024-12-14 14:29:37 +0800

    feat: add date and time helper

---
+++ src/helper/DateAndTimeHelper.ts
@@ -0,0 +1,10 @@
+export function getLocalTimeFormat(time: Date) {
+  return time.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
+}
