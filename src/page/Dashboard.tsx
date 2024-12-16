ebb3ab5703b4114ff96d132b345f445e355eac42
holifieldw <holifield04@gmail.com>
2024-12-16 17:07:44 +0800

    feat: WIP update dashboard page and styling

--- src/page/Dashboard.tsx
+++ src/page/Dashboard.tsx
@@ -1,30 +1,46 @@
+import CustomChart from "@/component/chart/CustomChart";
 import "./Dashboard.css";
-import Card from "../component/card/Card";
+import Card from "@/component/card/Card";
+import Table from "@/component/table/Table";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [isTableFullScreen, setIsTableFullScreen] = useState<boolean>(false);
   return (
     <>
       <div className="dashboard">
-        <Card
-          title="Total Revenue"
-          content="$11,5237"
-          description="From last month"
-        />
-        <Card
-          title="Total Revenue"
-          content="$11,5237"
-          description="From last month"
-        />
-        <Card
-          title="Total Revenue"
-          content="$11,5237"
-          description="From last month"
-        />
-        <Card
-          title="Total Revenue"
-          content="$11,5237"
-          description="From last month"
-        />
+        <div className={`cards ${isTableFullScreen === true && "notVisible"}`}>
+          <Card
+            title="Total Revenue"
+            content="$11,5237"
+            description="From last month"
+          />
+          <Card
+            title="Total Revenue"
+            content="$11,5237"
+            description="From last month"
+          />
+          <Card
+            title="Total Revenue"
+            content="$11,5237"
+            description="From last month"
+          />
+          <Card
+            title="Total Revenue"
+            content="$11,5237"
+            description="From last month"
+          />
+        </div>
+        <div className={`contents ${isTableFullScreen === true && "isFullScreen"}`}>
+          <CustomChart />
+          <Table
+            title="Sales orders"
+            badgeColumn="status"
+            boldColumn="orderId"
+            isFullScreen={isTableFullScreen}
+            onViewAll={() => setIsTableFullScreen(!isTableFullScreen)}
+          />
+        </div>
       </div>
     </>
   );
