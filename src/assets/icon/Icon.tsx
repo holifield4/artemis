2279f9eb17b7434ca0b3f030d085f42eac02711f
holifieldw <holifield04@gmail.com>
2024-12-18 21:38:09 +0800

    chore: removed unused line in icon selector

--- src/assets/icon/Icon.tsx
+++ src/assets/icon/Icon.tsx
@@ -144,8 +144,6 @@ export default function Icon({ icon }: IconProps) {
         <svg
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
           fill="none"
           viewBox="0 0 24 24"
           style={{ height: "16px", width: "16px" }}
@@ -164,8 +162,6 @@ export default function Icon({ icon }: IconProps) {
         <svg
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
           fill="none"
           viewBox="0 0 24 24"
           style={{ height: "16px", width: "16px" }}
@@ -188,8 +184,6 @@ export default function Icon({ icon }: IconProps) {
         <svg
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
           fill="none"
           viewBox="0 0 24 24"
           style={{ height: "16px", width: "16px" }}
@@ -209,8 +203,6 @@ export default function Icon({ icon }: IconProps) {
         <svg
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
           fill="none"
           viewBox="0 0 24 24"
           style={{ height: "16px", width: "16px" }}
@@ -230,8 +222,6 @@ export default function Icon({ icon }: IconProps) {
         <svg
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
           fill="none"
           viewBox="0 0 24 24"
           style={{ height: "16px", width: "16px" }}
@@ -251,8 +241,6 @@ export default function Icon({ icon }: IconProps) {
         <svg
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
           fill="none"
           viewBox="0 0 24 24"
           style={{ height: "16px", width: "16px" }}
@@ -272,8 +260,6 @@ export default function Icon({ icon }: IconProps) {
         <svg
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
           fill="currentColor"
           viewBox="0 0 24 24"
           style={{ height: "16px", width: "16px" }}
@@ -291,8 +277,6 @@ export default function Icon({ icon }: IconProps) {
         <svg
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
           fill="none"
           viewBox="0 0 24 24"
           style={{ height: "16px", width: "16px" }}
@@ -303,6 +287,24 @@ export default function Icon({ icon }: IconProps) {
             strokeLinejoin="round"
             strokeWidth="2"
             d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"
+          />
+        </svg>
+      );
+    }
+    case "Bar": {
+      return (
+        <svg
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+          style={{ height: "24px", width: "24px" }}
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeWidth="2"
+            d="M5 7h14M5 12h14M5 17h14"
           />
         </svg>
       );
